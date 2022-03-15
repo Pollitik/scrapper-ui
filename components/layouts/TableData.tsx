f1bd7d020b5bd5bbf7c8e3cfad6498e9e1dda869
alad2121 <alero.adegoroye@gmail.com>
2022-03-14 20:31:01 -0400

    Fixed a bug relating to the folder dropdown functionality

--- components/layouts/TableData.tsx
+++ components/layouts/TableData.tsx
@@ -34,7 +34,7 @@ const TableData: React.FC<Props> = ({ data, id, countries }) => {
   const currentDraggedItem = useRef("");
   const currentFilter = useRef("");
   const numsRef = useRef<Array<Number[]>>([]);
-  const selectedFolder = useRef<string>("1Xb4zAkP6ytMvqVPFTFVxfdgM5bKeVGB0");
+  const selectedFolder = useRef<string>("0B1t8CP92v4NSOUExcVFpNjBlZGs");
 
   const currentStyle = useRef("");
 
