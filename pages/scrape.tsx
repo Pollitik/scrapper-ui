3c2973afda34819c23a96b8f51391feb6a0c31e5
alad2121 <alero.adegoroye@gmail.com>
2022-04-25 18:05:27 -0400

    More Changes

--- pages/scrape.tsx
+++ pages/scrape.tsx
@@ -110,7 +110,7 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
     return data;
   });
 
-  const res = await axios.post("https://pollitik-scrapper.herokuapp.com//api/googledrive", {
+  const res = await axios.post("https://pollitik-scrapper.herokuapp.com/api/googledrive", {
     query: "'0B1t8CP92v4NSdnRGMVR0Y3NKckE'" + " in parents",
   });
 
