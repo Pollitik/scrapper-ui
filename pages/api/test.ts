488a46af8da6f7cfb2a11017bce45f02771f70b1
alad2121 <alero.adegoroye@gmail.com>
2022-04-26 16:34:06 -0400

    More Changes

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -3,10 +3,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(res:NextApiResponse, req:NextApiRequest){
 
-
-    if(req.method !== "POST")
-        res.status(404).send("404");
-
     
-    res.status(200).send("Great");
+
+    if(req.method == "GET"){
+        res.json({data:"Server"})
+        res.status(200).send("GET");
+    }
+    else if(req.method == "POST")
+        res.status(200).send("POST");
+    else
+        res.status(404).send("404");
 }
