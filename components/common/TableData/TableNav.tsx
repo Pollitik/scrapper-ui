fa26b75e9568a159b780596cca388eeed9a6b51f
alad2121 <alero.adegoroye@gmail.com>
2022-04-30 14:21:46 -0400

    Fixed bugs relating to the table naviagation

--- components/common/TableData/TableNav.tsx
+++ components/common/TableData/TableNav.tsx
@@ -7,26 +7,28 @@ interface Props {
 }
 
 const TableNav: React.FC<Props> = ({ tables, folders }) => {
-  const tableIndex = useRef<String>("");
+  // const tableIndex = useRef<String>("");
+  const [tableIndex, setTableIndex] = useState<String>("");
   const [isRender, renderTable] = useState(false);
 
 
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-col w-100 justify-center items-center">
-        <div className={`grid grid-cols-${Math.round(tables.length / 5)} grid-flow-row place-items-center`}>
+        <div className={`grid grid-cols-6 grid-flow-row`}>
           {tables?.map((table, index: number) => {
             return (
               <button
-                className="px-4 py-1 text-sm text-blue-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2"
+                className={`px-4 py-1 text-sm text-black-600 ${table[0].length <= 3 ? "bg-[red]" : ""} font-semibold rounded-full border  hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2`}
                 key={index}
-                title={table[1] == "" ? "Bad" : table[1]}
+                title={table.length <= 3 ? "No data" : table[1]}
                 onClick={() => {
                   renderTable(false);
-                  tableIndex.current = String(index);
+                  setTableIndex(String(index));
                 }}
               >
                 {`table-${index + 1}`}
+               
               </button>
             );
           })}
@@ -41,12 +43,13 @@ const TableNav: React.FC<Props> = ({ tables, folders }) => {
           Refresh
         </button>
 
-        <h1>Last rendered table: {`table-${tableIndex.current === "" ? "" : Number(tableIndex.current) + 1}`}</h1>
+        <h1>Last clicked table: {`table-${tableIndex === "" ? "" : Number(tableIndex) + 1}`}</h1>
+        {console.log(tables[Number(tableIndex)])}
       </div>
       <br />
       {isRender && (
         <TableData
-          data={tables[Number(tableIndex.current)]}
+          data={tables[Number(tableIndex)]}
           countries={folders}
           id={String(tableIndex)}
         />
