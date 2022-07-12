fecf1d9e36518e8220bb29246c3969e3f1e134a3
alad2121 <alero.adegoroye@gmail.com>
2022-07-12 13:14:04 -0400

    Organizing code

--- components/common/TableData/index.tsx
+++ components/common/TableData/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, ReactEventHandler } from "react";
+import React, { useState, useRef, useEffect} from "react";
 import TableRow from "#/components/common/TableRow";
 import LoadingIcon from "../LoadingIcon";
 import axios from "axios";
